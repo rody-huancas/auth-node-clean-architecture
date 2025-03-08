3340da6f15b158c2e02bb4358211068fb9218262
Rody Huancas <rodyhuancas.04@gmail.com>
2025-03-07 21:55:19 -0500

    Feat: Refactor authentication datasource to use hash and compare functions, and add user mapper for entity transformation

--- src/infraestructure/datasources/auth.datasource.impl.ts
+++ src/infraestructure/datasources/auth.datasource.impl.ts
@@ -1,8 +1,18 @@
 import { BcryptAdapter } from "../../config";
 import { UserModel } from "../../data/mongodb";
 import { AuthDatasource, CustomError, RegisterUserDto, UserEntity } from "../../domain";
+import { UserMapper } from "../mappers/user.mapper";
+
+type HashFunction = (password: string) => string;
+type CompareFunction = (password: string, hashed: string) => boolean;
 
 export class AuthDatasourceImpl implements AuthDatasource {
+
+  constructor(
+    private readonly hashPassword: HashFunction = BcryptAdapter.hash,
+    private readonly comparePassword: CompareFunction = BcryptAdapter.compare
+  ) {}
+
   login(email: string, password: string): Promise<any> {
     throw new Error("Method not implemented.");
   }
@@ -19,13 +29,13 @@ export class AuthDatasourceImpl implements AuthDatasource {
       const user = await UserModel.create({
         name,
         email,
-        password: BcryptAdapter.hash(password),
+        password: this.hashPassword(password),
       });
 
       await user.save();
 
       // 3. Mapear la data y crear el usuario
-      return new UserEntity(user.id, name, email, user.password, user.roles);
+      return UserMapper.userEntityFromObject(user);
 
       // 4. Crear el JWT
       // 5. Devolver el JWT
